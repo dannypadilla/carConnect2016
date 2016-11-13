b4d4eec78d30c861ed7e84de6ae030af1acfddc9
Fernando <fernandomontes91@me.com>
2016-11-13 12:14:44 -0800

    uploaded old index. Got alexa to talk.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var Alexa = require('alexa-sdk');
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 var MojioClientLite = require('mojio-client-lite');
+var SKILL_NAME = "Car Check"
 
 var config = {
     application: "088251a7-b45c-489b-829f-b6b71eefa6ae",
@@ -11,34 +12,10 @@ var config = {
 
 var mojio_client = new MojioClientLite(config);
 
-mojio_client.authorize('disavowed10@gmail.com','fernieLand69').then(function(res,err) {
-
-    if(typeof(err)!="undefined")
-    {
-        console.log("login error");
-        return;
-    }
-
-    // login successful
-    // write your logic here
-
-    mojio_client.get().vehicles().then(function(res,err){
-        // if err is null then data will be inside res
-        var index = 0
-        console.log(res.Data[index]);
-        console.log(res.Data[index].Name);
-	    console.log(res.Data[index].FuelEfficiency);
-
-    });
-
-});
-
-
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
+    // To enable string internationalization (i18n) features, set a resources object
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
@@ -48,12 +25,42 @@ var handlers = {
         this.emit('CarStatus');
     },
     'CarStatus': function () {
-        // Get a random space fact from the space facts list
-        // Use this.t() to get corresponding language data
+        var stuff= "herh";
+        var parentOfThis=this;
+       // this.emit(':tell',"test");
+        mojio_client.authorize('disavowed10@gmail.com','fernieLand69').then(function(res,err) {
 
-        // Create speech output
-        var speechOutput;
-        this.emit(':tell', speechOutput)
+            if(typeof(err)!="undefined")
+            {
+                console.log("login error");
+                return;
+            }
+            // login successful
+            // write your logic 
+            console.log("auth success");
+
+            mojio_client.get().vehicles().then(function(res,err){
+                if(typeof(err)!="undefined")
+            {
+                console.log("call error");
+                return;
+            }
+                // if err is null then data will be inside res
+                //this.emit(':tell', "stuff");
+                //console.log("fuel efficiency: "+ res.Data[0].FuelEfficiency.Value.toString());
+                //stuff = res.Data[0].FuelEfficiency.Value.toString();
+                //console.log("status: "+status);
+                console.log("in client get");
+                parentOfThis.emit(':ask',res.Data[0].FuelEfficiency.Value.toString());
+                console.log("after");
+
+            }
+            //, () => {
+            //this.emit(':tell', stuff);
+    //}
+    );
+
+});
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = this.t("HELP_MESSAGE");
